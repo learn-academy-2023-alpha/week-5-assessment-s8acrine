8c050f05f70c7d06373c561a979b988f0de1da15
Aaron Criner <aaron@ACMBP.local>
2023-03-17 14:05:32 -0700

    challenges in progress

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,14 +12,49 @@
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
+// Pseudocode:
+// Create a function leetSpeakify
+// Input: A string
+// Output: A string with the following modifications:
+// a -> 4
+// e -> 3
+// i -> 1
+// o -> 0
+// Can use string.replace method multiple times to swich each character. This seems memory intensive, so let's go with a simple set of if then loops.
+// First, take the string and turn it into an array with .split("") to seperate each character into it's own index in the array
+// Now we can .map over the array taking in value,index, and array as arguments, taking each modification as an if statement.
+// if the character is one of the modified characters (make sure to account for uppercase with || ), we can set the value at the index to the modified value.
+// Once the loop has finished, we will want to take the mapped array and turn it back into a string with .join("") and then return.
+
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+describe("leetSpeakify", () => {
+  const secretCodeWord1 = "Lackadaisical"; // Expected output: "L4ck4d41s1c4l"
+  const secretCodeWord2 = "Gobbledygook"; // Expected output: "G0bbl3dyg00k"
+  const secretCodeWord3 = "Eccentric"; // Expected output: "3cc3ntr1c"
+  it("Modifies a, e, i, and o characters in a string to a number", () => {
+    expect(leetSpeakify(secretCodeWord1)).toEqual("L4ck4d41s1c4l");
+    expect(leetSpeakify(secretCodeWord2)).toEqual("G0bbl3dyg00k");
+    expect(leetSpeakify(secretCodeWord3)).toEqual("3cc3ntr1c");
+  });
+});
+
+const leetSpeakify = (string) => {
+  string
+    .split("")
+    .map((v, i, a) => {
+      if (v === "a" || "A") {
+        a[i] = "4";
+      } else if (v === "e" || "E") {
+        a[i] = "3";
+      } else if (v === "i" || "I") {
+        a[i] = "1";
+      } else if (v === "o" || "O") {
+        a[i] = "0";
+      }
+    })
+    .join("");
+};
 
 // b) Create the function that makes the test pass.
 
@@ -27,11 +62,11 @@ const secretCodeWord3 = "Eccentric"
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"];
 
-const letterA = "a"
+const letterA = "a";
 // Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
+const letterE = "e";
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // b) Create the function that makes the test pass.
@@ -40,13 +75,13 @@ const letterE = "e"
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
+const hand1 = [5, 5, 5, 3, 3];
 // Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
+const hand2 = [5, 5, 3, 3, 4];
 // Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
+const hand3 = [5, 5, 5, 5, 4];
 // Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
+const hand4 = [7, 2, 7, 2, 7];
 // Expected output: true
 
 // b) Create the function that makes the test pass.
